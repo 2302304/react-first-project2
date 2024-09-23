6eecc7c06cb38229cd98a170ace5a66389f7e04b
Jani Harju <e2302304@edu.vamk.fi>
2024-09-23 15:54:05 +0300

    Lisätty NewExpense-komponentti ja Expenses-komponentin import App.js:ään

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import Expenses from '../Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 
 
@@ -36,6 +36,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
       <p>Jani Harju / VAMK / BIT</p>
     </div>
